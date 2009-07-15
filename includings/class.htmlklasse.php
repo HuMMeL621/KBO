bf9c8554efe4bd4558042f09acc38e3957869bdc
root <root@Sephiroth.(none)>
2009-07-15 11:23:29 +0200

    merge numero 2

---
+++ includings/class.htmlklasse.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+class HtmlKlasse
+{
+	public $klassens;
+	
+	public function __construct($klassens)
+	{
+		$this ->klassens = $klassens;
+	}
+	
+	public function getKlassens()
+	{
+		$html = '';
+		foreach ($this->klassens as $klasse)
+		{
+			$html.= '	<tr>
+		<td><input type="text" name="klasse'.$klasse['klasse_id'].'" value="'.$klasse['name'].'"></td>
+		<td><input type="submit" name="" value="deaktivieren"></td>
+		<td><input type="submit" name="update" value="Update"></td>
+	</tr>';
+			
+		}
+	return $html;
+	}
+	
+}
+
+?>
